e0dccf4b854f178e4e96662c04610a72f9ee598a
Your Name <debduttan.mech.ug@jadavpuruniversity.in>
2025-03-29 13:12:11 +0530

    Update route.ts

--- app/api/novels/new/route.ts
+++ app/api/novels/new/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { connectToDatabase } from '@/lib/mongodb';
+import { connectToDatabase } from '@/app/lib/mongodb';
 import { ObjectId } from 'mongodb';
 
 export const dynamic = 'force-dynamic';
